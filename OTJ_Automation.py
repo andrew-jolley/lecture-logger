1cafb348aa207a0d17328df41e09e0590d7de4fb
liamshadwell <liam.shadwell@bbc.co.uk>
2025-09-29 19:50:31 +0100

    Added input validation for file path entered with ""
    When copying the 'File as Path' in windows, it automatically adds "" around the path. This is now removed with this addition, to avoid the program crashing.

--- OTJ_Automation.py
+++ OTJ_Automation.py
@@ -11,7 +11,7 @@ def path():
     name = "FilePath.txt"   #set name of File Path config txt file
     path = ""   #set path to blank string
     try: 
-        path = readTXT(name)[0].replace("\n","")    #set path as the name specified in the txt file
+        path = readTXT(name)[0].replace("\n","").replace('"','')    #set path as the name specified in the txt file
     except Exception:   #error handline
         fatal(f"path() - Cannot find '{name}'")   #this error is fatal, so fatal() is used
     return path     #return path (will be blank due to path = "" if there is exception)
